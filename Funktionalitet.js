0485787c3ae64d78507b010c5fd6b368117098ee
Heimbrand <olle.heimbrand@hotmail.se>
2024-02-06 15:17:49 +0100

    fixed some functions and design

--- Funktionalitet.js
+++ Funktionalitet.js
@@ -1,1 +1,30 @@
-const services = [Pälsvård, ]
+const services = 
+[
+ {service: "Kloklippning", image: "Kloklippning.jpg", price: 150},
+ {service: "Pälsvård", image: "Pälsvårdad.jpg", price: 200},
+ {service: "Umgås med kammen", image: "Redo för att kammas.jpg", price: 50}
+];
+
+
+const myServices = document.querySelector(".product-section");
+
+for (let i = 0; i < services.length; i++)
+{
+     const service = services[i];
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    const productImage = document.createElement("img");
+    productImage.src = service.image;
+
+    const productName = document.createElement("h3");
+    productName.textContent = service.service;
+
+    const productPrice = document.createElement("p");
+    productPrice.textContent = service.price;
+
+    productCard.appendChild(productImage);
+    productCard.appendChild(productName);
+    productCard.appendChild(productPrice);
+    myServices.appendChild(productCard);
+}
