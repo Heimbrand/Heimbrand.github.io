13bd58a510a5f19ffa34b9cd60dea943bdbaf525
Heimbrand <Olle.Heimbrand@hotmail.se>
2024-02-07 11:53:30 +0100

    bootstraphjälp av niklas: katt img

--- Funktionalitet.js
+++ Funktionalitet.js
@@ -1,30 +1,27 @@
-const services = 
-[
- {service: "Kloklippning", image: "Kloklippning.jpg", price: 150},
- {service: "Pälsvård", image: "Pälsvårdad.jpg", price: 200},
- {service: "Umgås med kammen", image: "Redo för att kammas.jpg", price: 50}
+const services = [
+  { service: "Kloklippning", image: "Kloklippning.jpg", price: 150 },
+  { service: "Pälsvård", image: "Pälsvårdad.jpg", price: 200 },
+  { service: "Umgås med kammen", image: "Redo för att kammas.jpg", price: 50 },
 ];
-
 
 const myServices = document.querySelector(".product-section");
 
-for (let i = 0; i < services.length; i++)
-{
-     const service = services[i];
-    const productCard = document.createElement("div");
-    productCard.classList.add("product-card");
+for (let i = 0; i < services.length; i++) {
+  const service = services[i];
+  const productCard = document.createElement("div");
+  productCard.classList.add("product-card");
 
-    const productImage = document.createElement("img");
-    productImage.src = service.image;
+  const productImage = document.createElement("img");
+  productImage.src = service.image;
 
-    const productName = document.createElement("h3");
-    productName.textContent = service.service;
+  const productName = document.createElement("h3");
+  productName.textContent = service.service;
 
-    const productPrice = document.createElement("p");
-    productPrice.textContent = service.price;
+  const productPrice = document.createElement("p");
+  productPrice.textContent = service.price;
 
-    productCard.appendChild(productImage);
-    productCard.appendChild(productName);
-    productCard.appendChild(productPrice);
-    myServices.appendChild(productCard);
+  productCard.appendChild(productImage);
+  productCard.appendChild(productName);
+  productCard.appendChild(productPrice);
+  myServices.appendChild(productCard);
 }
